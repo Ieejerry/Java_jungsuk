0abd53321d32dfc88806f64abacab106c00425a0
Hyeonseong Lee <hslee7231@gmail.com>
2022-07-29 19:45:23 +0900

    변수의 선언과 저장

---
+++ ch02/src/Ex2_1.java
@@ -0,0 +1,18 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		
+		// sysout ctrl + space 자동완성
+		// ctrl + alt + down 행단위 복사
+		// alt + shift + a 칼럼 편집 모드(토글)
+		// print() 출력 후 줄바꿈을 안함
+		// println() 출력 후 줄바꿈을 함
+		
+		System.out.println(5 + 3);	// 덧셈
+		System.out.println(5 - 3);	// 뺄셈
+		System.out.println(5 * 3);	// 곱셈
+		System.out.println(5 / 3);	// 나눗셈
+	}
+
+}
