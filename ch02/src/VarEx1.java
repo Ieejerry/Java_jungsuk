b893c2730536d131f4c8925f8c68037cdaf40dc1
Hyeonseong Lee <hslee7231@gmail.com>
2022-07-29 19:45:47 +0900

    변수의 타입

---
+++ ch02/src/VarEx1.java
@@ -0,0 +1,13 @@
+
+public class VarEx1 {
+
+	public static void main(String[] args) {
+		
+		int x = 4, y = 2;	// 변수 x, y 선언 및 할당
+		System.out.println(x + y);	// 덧셈
+		System.out.println(x - y);	// 뺄셈
+		System.out.println(x * y);	// 곱셈
+		System.out.println(x / y);	// 나눗셈
+	}
+
+}
