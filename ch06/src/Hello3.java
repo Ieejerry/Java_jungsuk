159c978c3da2979ccc920374dd4c69ddb683d427
Hyeonseong Lee <hslee7231@gmail.com>
2022-09-04 17:45:43 +0900

    한 파일 여러 클래스 작성하기

---
+++ ch06/src/Hello3.java
@@ -0,0 +1,12 @@
+// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+class Hello2 {}	// 소스파일의 이름은 public class이름과 일치
+//public class Hello2 {}	// 소스파일의 이름은 public class이름과 일치
+class Hello3 {}
+////class  hello3 {	// 소스파일의 이름과 main 메소드가 들어있는 클래스의 이름이 일치하여야 한다.
+//	public static void main(String[] args) {
+//		System.out.println("hello");
+//	}
+//}
+//public class Hello3 {}	// 하나의 소스파일에는 하나의 public class만 허용
+class Hello4 {}
+// public class가 없는 경우에는 클래스들중 소스파일과 같은 이름의 클래스가 존재하면 된다.
