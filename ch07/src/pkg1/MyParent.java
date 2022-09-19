e9132668787214b872f0eda5491cf6f7d1388b56
Hyeonseong Lee <hslee7231@gmail.com>
2022-09-19 22:08:54 +0900

    접근제어자

---
+++ ch07/src/pkg1/MyParent.java
@@ -0,0 +1,25 @@
+package pkg1;
+
+public class MyParent {	// 접근제어자가 public
+	private int prv;	// 같은 클래스
+	int dft;	// 같은 패키지
+	protected int prt;	// 같은 패키지 + 자손(다른 패키지)
+	public int pub;	// 접근제한 없음
+	
+	public void printMembers() {
+		System.out.println(prv);	// OK
+		System.out.println(dft);	// OK
+		System.out.println(prt);	// OK
+		System.out.println(pub);	// OK
+	}
+}
+
+class MyParentTest {	// 접근제어자가 (default)
+	public static void main(String[] args) {
+		MyParent p = new MyParent();
+//		System.out.println(p.prv);	// 에러. 
+		System.out.println(p.dft);	// OK
+		System.out.println(p.prt);	// OK
+		System.out.println(p.pub);	// OK
+	}
+}
