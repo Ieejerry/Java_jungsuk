fcd2c1bce04831439841b4cc21aeb98f3b0dc91d
Hyeonseong Lee <hslee7231@gmail.com>
2022-09-19 21:30:49 +0900

    import문, static import문

---
+++ ch07/src/Ex7_6.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;	// java.lang.System의 static변수 out, 클래스 생략가능
+import static java.lang.Math.*;	// Math클래스의 모든 static 멤버
+
+public class Ex7_6 {
+	public static void main(String[] args) {
+		// System.out.pritln(Math.random());
+		out.println(random());
+		
+		// System.out.println("Math.PI : " + Math.PI);
+		out.println("Math.PI : " + PI);
+	}
+}
