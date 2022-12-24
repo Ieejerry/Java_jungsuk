66d8fd9be3015cb12ab6e766ec42431436410eed
Hyeonseong Lee <hslee7231@gmail.com>
2022-12-24 17:14:32 +0900

    스트림만들기

--- ch14/src/Ex14_0.java
+++ ch14/src/Ex14_0.java
@@ -1,36 +1,48 @@
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.*;
+import java.util.stream.*;
 
 public class Ex14_0 {
 	public static void main(String[] args) {
-//		Function<String, Integer> f = (String s) -> Integer.parseInt(s);
-//		Function<String, Integer> f = 클래스이름::메소드이름;
-		Function<String, Integer> f = Integer::parseInt;	// 메소드 참조
-		System.out.println(f.apply("100") + 200);
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		Stream<Integer> intStream = list.stream();	// list를 stream으로 변환
+		intStream.forEach(System.out::print);	// forEach() 최종연산
 		
-		// Supplier는 입력 X, 출력 O
-//		Supplier<MyClass> s = () -> new MyClass();
-//		Supplier<MyClass> s = MyClass::new;
+		// stream은 1회용. stream에 대해 최종연산을 수행하면 stream이 닫힌다.
+		intStream = list.stream();	// list로부터 stream을 생성
+		intStream.forEach(System.out::print);
 		
-//		Function<Integer, MyClass> f2 = (i) -> new MyClass(i);
-		Function<Integer, MyClass> f2 = MyClass::new;
+		System.out.println();
 		
-		MyClass mc = f2.apply(100);
-		System.out.println(mc.iv);
+//		Stream<String> strStream = Stream.of("a", "b", "c");
+//		Stream<String> strStream = Stream.of(new String[] {"a", "b", "c", "d"});
+		String[] strArr = {"a", "b", "c", "d"};
+//		Stream<String> strStream = Stream.of(strArr);
+		Stream<String> strStream = Arrays.stream(strArr);
+		strStream.forEach(System.out::println);
 		
-		System.out.println(f2.apply(200).iv);
+		int[] intArr = {1, 2, 3, 4, 5};
+		IntStream intStream2 = Arrays.stream(intArr);
+//		intStream2.forEach(System.out::println);
+//		System.out.println("count = " + intStream2.count());	// 최종연산
+//		System.out.println("sum = " + intStream2.sum());
+		System.out.println("average = " + intStream2.average());
 		
-//		Function<Integer, int[]> f3 = (i) -> new int[i];
-		Function<Integer, int[]> f3 = int[]::new;	// 메소드 참조
-		int[] arr = f3.apply(100);
-		System.out.println("arr.length = " + arr.length);
-	}
-}
-
-class MyClass {
-	int iv;
-	
-	MyClass(int iv) {
-		this.iv = iv;
+//		Integer[] intArr = {1, 2, 3, 4, 5};
+//		Stream<Integer> intStream2 = Arrays.stream(intArr);
+//		intStream2.forEach(System.out::println);
+//		System.out.println("count = " + intStream2.count());
+		
+//		IntStream intStream3 = new Random().ints(5, 10);	// 무한스트림
+//		intStream3.limit(10).forEach(System.out::println);	// 10개만 자르기
+		IntStream intStream3 = new Random().ints(10, 5, 10);	// 유한스트림
+		intStream3.forEach(System.out::println);
+		
+		// iterate(T seed, UnaryOperator f) 단항 연산자 
+		Stream<Integer> intStream4 = Stream.iterate(1, n -> n + 2);
+		intStream4.limit(10).forEach(System.out::println);
+		
+		// generate(Supplier s) : 주기만 하는 것. 입력 X, 출력 O
+		Stream<Integer> oneStream = Stream.generate(() -> 1);
+		oneStream.limit(10).forEach(System.out::println);
 	}
 }
