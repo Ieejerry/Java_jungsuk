e903d30528749a220d04e737cb52179e600f033c
Hyeonseong Lee <hslee7231@gmail.com>
2022-12-26 00:24:25 +0900

    Optional

--- ch14/src/Ex14_0.java
+++ ch14/src/Ex14_0.java
@@ -1,48 +1,28 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.Optional;
 
 public class Ex14_0 {
 	public static void main(String[] args) {
-		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-		Stream<Integer> intStream = list.stream();	// list를 stream으로 변환
-		intStream.forEach(System.out::print);	// forEach() 최종연산
+//		int[] arr = null;
+		int[] arr = new int[0];
+		System.out.println("arr.length = " + arr.length);
 		
-		// stream은 1회용. stream에 대해 최종연산을 수행하면 stream이 닫힌다.
-		intStream = list.stream();	// list로부터 stream을 생성
-		intStream.forEach(System.out::print);
+//		Optional<String> opt = null;	// OK. 하지만 바람직X
+		Optional<String> opt = Optional.empty();
+//		Optional<String> opt = Optional.of("abc");
+		System.out.println("opt = " + opt);
+//		System.out.println("opt = " + opt.get());
 		
-		System.out.println();
+		String str = "";
 		
-//		Stream<String> strStream = Stream.of("a", "b", "c");
-//		Stream<String> strStream = Stream.of(new String[] {"a", "b", "c", "d"});
-		String[] strArr = {"a", "b", "c", "d"};
-//		Stream<String> strStream = Stream.of(strArr);
-		Stream<String> strStream = Arrays.stream(strArr);
-		strStream.forEach(System.out::println);
+//		try {
+//			str = opt.get();
+//		} catch (Exception e) {
+//			str = "";	// 예외가 발생하면 빈문자열("")로 초기화
+//		}	// 예외 발생
 		
-		int[] intArr = {1, 2, 3, 4, 5};
-		IntStream intStream2 = Arrays.stream(intArr);
-//		intStream2.forEach(System.out::println);
-//		System.out.println("count = " + intStream2.count());	// 최종연산
-//		System.out.println("sum = " + intStream2.sum());
-		System.out.println("average = " + intStream2.average());
-		
-//		Integer[] intArr = {1, 2, 3, 4, 5};
-//		Stream<Integer> intStream2 = Arrays.stream(intArr);
-//		intStream2.forEach(System.out::println);
-//		System.out.println("count = " + intStream2.count());
-		
-//		IntStream intStream3 = new Random().ints(5, 10);	// 무한스트림
-//		intStream3.limit(10).forEach(System.out::println);	// 10개만 자르기
-		IntStream intStream3 = new Random().ints(10, 5, 10);	// 유한스트림
-		intStream3.forEach(System.out::println);
-		
-		// iterate(T seed, UnaryOperator f) 단항 연산자 
-		Stream<Integer> intStream4 = Stream.iterate(1, n -> n + 2);
-		intStream4.limit(10).forEach(System.out::println);
-		
-		// generate(Supplier s) : 주기만 하는 것. 입력 X, 출력 O
-		Stream<Integer> oneStream = Stream.generate(() -> 1);
-		oneStream.limit(10).forEach(System.out::println);
+//		str = opt.orElse("EMPTY");	// Optional에 저장된 값이 null이면 ""반환
+//		str = opt.orElseGet(() -> new String());	// Optional에 저장된 값이 null이면 ""반환
+		str = opt.orElseGet(String::new);	// Optional에 저장된 값이 null이면 ""반환
+		System.out.println("str = " + str);
 	}
 }
